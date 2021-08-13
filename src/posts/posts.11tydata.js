dcf22b963be6bfb3f781c3dfb7b9843e025f932a
Thomas Michael Semmler <contact@thomassemmler.com>
2021-08-14 00:39:33 +0200

    add a mechanism to enable drafts in blog posts

---
+++ src/posts/posts.11tydata.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const isDevEnv = process.env.ENV === 'development';
+const todaysDate = new Date();
+
+function showDraft(data) {
+	const isDraft = 'draft' in data && data.draft !== false;
+	const isFutureDate = data.page.date > todaysDate;
+	return isDevEnv || (!isDraft && !isFutureDate);
+}
+
+module.exports = function() {
+	return {
+		eleventyComputed: {
+			eleventyExcludeFromCollections: function(data) {
+				if(showDraft(data)) {
+					return data.eleventyExcludeFromCollections;
+				}
+				else {
+					return true;
+				}
+			},
+			permalink: function(data) {
+				if(showDraft(data)) {
+					return 'blog/{{ date | dateGetYear }}/{{ title | slug }}/'
+				}
+				else {
+					return false;
+				}
+			},
+            tags: 'post',
+            layout: 'post'
+		}
+	}
+}
