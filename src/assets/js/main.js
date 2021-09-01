be8229d79aae240c0570cb8252cdde4b8b428825
Thomas Michael Semmler <contact@thomassemmler.com>
2021-09-01 21:09:05 +0200

    switch from :target based menu to js driven menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,9 @@
 import { colorSettings } from "./modules/colorSettings.js";
 
 const root = document.documentElement;
+const openMenuButton = document.querySelector('[data-open-site-menu]');
 const closeMenuButton = document.querySelector('[data-close-site-menu]');
-const menu = document.querySelector('[data-menu]');
+const menu = document.querySelector('[data-site-menu]');
 const accentColorSettings = document.querySelectorAll('[data-accent-color-settings]');
 const accentOverride = sessionStorage['accent-color-override'];
 const footerSettingsButton = document.querySelector('[data-toggle-footer-settings]');
@@ -11,23 +12,34 @@ const favicons = document.querySelectorAll('[data-favicon]');
 // adding a Paint Worklett allowing corner smoothing with CSS border-radius
 (CSS.paintWorklet || paintWorklet).addModule('/assets/js/vendor/smooth-corners.js')
 
-// make the closing of the menu nicer
+// open the menu
+openMenuButton.addEventListener('click', e => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if ( !menu.classList.contains('is-open') ) {
+        menu.classList.add('is-open');
+        e.currentTarget.setAttribute('aria-expanded', true);
+    }
+});
+
+// close the menu
 closeMenuButton.addEventListener('click', e => {
     e.stopPropagation();
     e.preventDefault();
-    const href = e.currentTarget.href; // makes sure that it uses the target of the element firing the event, not its children
-    const fragment = href.substring(href.indexOf('#')); // wherever the link leads to...
 
     menu.classList.add('moveout'); // visually move the menu out
     menu.addEventListener('transitionend', function moveout(e) {
         e.stopPropagation;
+        e.currentTarget.classList.remove('is-open');
         e.currentTarget.classList.remove('moveout'); // remove the class for animation
-        window.location.hash = fragment; // set the new target
+
+        openMenuButton.setAttribute('aria-expanded', false);
+        //menu.classList.remove('is-open');
         // this is important, otherwise the menu will keep "closing" itself if its being opened again, ebcause the transitionend event triggers again
-        setTimeout(() => window.scrollTo(0,0)); // to prevent the jumping to the target
         menu.removeEventListener('transitionend', moveout);
     })
-});
+})
 
 // allow overriding the accent color
 colorSettings({ root, accentColorSettings, accentOverride, footerSettingsButton, favicons });
