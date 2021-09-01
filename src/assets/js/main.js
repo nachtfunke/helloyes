b356f39921bd0cd0bcd3a442226e6fdb5b05ed73
Thomas Michael Semmler <contact@thomassemmler.com>
2021-09-01 21:48:04 +0200

    improve the accessibility of the menu overlay and the color settings

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,9 @@
 import { colorSettings } from "./modules/colorSettings.js";
+import { focusFirstIn } from "./util/getFocusable.js";
 
-const root = document.documentElement;
 const openMenuButton = document.querySelector('[data-open-site-menu]');
 const closeMenuButton = document.querySelector('[data-close-site-menu]');
 const menu = document.querySelector('[data-site-menu]');
-const accentColorSettings = document.querySelectorAll('[data-accent-color-settings]');
-const accentOverride = sessionStorage['accent-color-override'];
-const footerSettingsButton = document.querySelector('[data-toggle-footer-settings]');
-const favicons = document.querySelectorAll('[data-favicon]');
 
 // adding a Paint Worklett allowing corner smoothing with CSS border-radius
 (CSS.paintWorklet || paintWorklet).addModule('/assets/js/vendor/smooth-corners.js')
@@ -20,6 +16,7 @@ openMenuButton.addEventListener('click', e => {
     if ( !menu.classList.contains('is-open') ) {
         menu.classList.add('is-open');
         e.currentTarget.setAttribute('aria-expanded', true);
+        focusFirstIn(menu);
     }
 });
 
@@ -35,6 +32,7 @@ closeMenuButton.addEventListener('click', e => {
         e.currentTarget.classList.remove('moveout'); // remove the class for animation
 
         openMenuButton.setAttribute('aria-expanded', false);
+        openMenuButton.focus();
         //menu.classList.remove('is-open');
         // this is important, otherwise the menu will keep "closing" itself if its being opened again, ebcause the transitionend event triggers again
         menu.removeEventListener('transitionend', moveout);
@@ -42,4 +40,4 @@ closeMenuButton.addEventListener('click', e => {
 })
 
 // allow overriding the accent color
-colorSettings({ root, accentColorSettings, accentOverride, footerSettingsButton, favicons });
+colorSettings();
